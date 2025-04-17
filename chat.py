499fe12493a7c18efa54f52768ace9c2ef88a25e
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-17 11:30:39 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ import torch
 import pickle
 from torch import nn
 from torch.optim import Adam
-from seq2seq import Seq2Seq
+from Seq2Seq import Seq2Seq
 from training_data import get_data
 
 # Hyperparameters
