8cbdf0fa1769402c333b3f93b118005a88b94a35
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-17 10:59:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 import pickle
 from torch import nn
 from torch.optim import Adam
-from Seq2Seq import seq2seq
+from Seq2Seq import seq2seq as Seq2Seq
 from training_data import get_data
 
 # Hyperparameters
