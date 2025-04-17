1ace6024b50fd1a44afd16ab7138fd4bea2b0e25
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-17 16:09:55 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -15,8 +15,8 @@ def get_ds():
 
     for line in lines:
         input_line, target_line = line.split('\t')
-        input_tensor.append([int(re.sub(r'\D', '', word)) for word in input_line.split() if re.sub(r'\D', '', word) != ''])
-        target_tensor.append([int(word) for word in target_line.split()])
+        # Alleen numerieke waarden toevoegen aan target_tensor
+        target_tensor.append([int(re.sub(r'\D', '', word)) for word in target_line.split() if re.sub(r'\D', '', word) != ''])
         vocab.update(input_line.split())
         vocab.update(target_line.split())
 
