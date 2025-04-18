27b71281ff444bcf96bdc50c68573232d1780564
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-18 14:30:47 +0200

    Update model.py

--- model.py
+++ model.py
@@ -5,18 +5,21 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
 from tensorflow.keras.callbacks import EarlyStopping
+import os
 
 # Laad de dataset
 def load_dataset(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.readlines()
 
-# Verwerk de dataset en maak sequenties van woorden
+# Preprocessen van de tekst om sequenties van woorden te maken
 def preprocess_data(corpus):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(corpus)
-    total_words = len(tokenizer.word_index) + 1  # Om rekening te houden met 0-index
+    total_words = len(tokenizer.word_index) + 1
     input_sequences = []
+
+    # Maak sequenties van woorden
     for line in corpus:
         token_list = tokenizer.texts_to_sequences([line])[0]
         for i in range(1, len(token_list)):
@@ -26,9 +29,8 @@ def preprocess_data(corpus):
     max_sequence_length = max([len(seq) for seq in input_sequences])
     input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
 
-    X = input_sequences[:, :-1]  # Alle woorden behalve het laatste woord
-    y = input_sequences[:, -1]   # Het laatste woord is het doel (volgende woord)
-
+    X = input_sequences[:, :-1]
+    y = input_sequences[:, -1]
     y = tf.keras.utils.to_categorical(y, num_classes=total_words)
 
     return X, y, tokenizer, total_words, max_sequence_length
@@ -39,30 +41,31 @@ def build_model(total_words, max_sequence_length):
     model.add(Embedding(total_words, 50, input_length=max_sequence_length - 1))
     model.add(LSTM(100))
     model.add(Dense(total_words, activation='softmax'))
-
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-# Train het model
+# Train het model en sla het op
 def train_model():
-    # Laad en verwerk de dataset
-    corpus = load_dataset('data/dataset.txt')
+    corpus = load_dataset('dataset.txt')
     X, y, tokenizer, total_words, max_sequence_length = preprocess_data(corpus)
-
-    # Bouw het model
     model = build_model(total_words, max_sequence_length)
 
-    # Voeg EarlyStopping toe voor de training
     early_stopping = EarlyStopping(monitor='loss', patience=5, restore_best_weights=True)
-
-    # Train het model
     model.fit(X, y, epochs=100, verbose=1, callbacks=[early_stopping])
 
+    # Verkrijg het absolute pad naar de root directory van je project
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(base_path, 'model', 'my_model.h5')
+
     # Sla het model op
-    model.save('model/my_model.h5')
-    return model, tokenizer
+    model.save(model_path)
+
+    # Sla de tokenizer op als pickle bestand
+    with open(os.path.join(base_path, 'model', 'tokenizer.pkl'), 'wb') as tokenizer_file:
+        import pickle
+        pickle.dump(tokenizer, tokenizer_file)
+
+    print("Model en tokenizer zijn succesvol getraind en opgeslagen!")
 
 if __name__ == '__main__':
-    # Train en sla het model op
-    model, tokenizer = train_model()
-    print("Model en tokenizer zijn succesvol getraind en opgeslagen!")
+    train_model()
