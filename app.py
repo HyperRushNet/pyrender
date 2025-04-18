7ad896cdfe30ffb2768a1c5d0942ff62e5f7fdd4
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-18 14:30:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
-from flask import Flask, request, jsonify
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import numpy as np
+import os
+from flask import Flask, request, jsonify
 
-# Initialiseer Flask app
+# Verkrijg het absolute pad naar de root directory van je project
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Laad het model
+model_path = os.path.join(base_path, 'model', 'my_model.h5')
+model = tf.keras.models.load_model(model_path)
+
+# Laad de tokenizer
+with open(os.path.join(base_path, 'model', 'tokenizer.pkl'), 'rb') as f:
+    tokenizer = pickle.load(f)
+
+# Flask applicatie
 app = Flask(__name__)
 
-# Laad je getrainde model (zorg ervoor dat je model bestand hebt)
-model = tf.keras.models.load_model('model/my_model.h5')
+# Functie om voorspellingen te doen
+def predict_next_word(input_text):
+    token_list = tokenizer.texts_to_sequences([input_text])[0]
+    token_list = pad_sequences([token_list], maxlen=20, padding='pre')  # Padding
+    pred = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(pred)
+    predicted_word = tokenizer.index_word.get(predicted_word_index, None)
+    return predicted_word
 
-# Laad je tokenizer
-tokenizer = Tokenizer()
-
-# Functie om het volgende woord te voorspellen
-def predict_next_word(text):
-    token_list = tokenizer.texts_to_sequences([text])[0]
-    token_list = pad_sequences([token_list], maxlen=10, padding='pre')
-    predicted = model.predict_classes(token_list, verbose=0)
-    return tokenizer.index_word[predicted[0]]
-
-# API endpoint voor voorspelling via queryparameter
+# Route om voorspellingen te doen
 @app.route('/predict', methods=['GET'])
 def predict():
-    # Haal de tekst op uit de URL parameter 'q'
-    input_text = request.args.get('q', '')  # Default naar lege string als er geen parameter is
-    if input_text == '':
-        return jsonify({"error": "Missing query parameter 'q'"}), 400
-    
-    next_word = predict_next_word(input_text)
-    return jsonify({"input_text": input_text, "next_word": next_word})
+    query = request.args.get('q', '')  # Haal de queryparameter op
+    next_word = predict_next_word(query)  # Voorspel het volgende woord
+    return jsonify({'next_word': next_word})
 
-# Start de app
 if __name__ == '__main__':
     app.run(debug=True)
