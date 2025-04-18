3ff6856aba6dacf872d6f9595939fa2cde5664b2
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-18 11:01:36 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 import torch
-import re
+from torch.nn.utils.rnn import pad_sequence
 import requests
+import re
 
 def get_ds():
     url = 'https://hyperrushnet.github.io/ai-training/data/ds1.txt'
@@ -11,24 +12,39 @@ def get_ds():
 
     input_tensor = []
     target_tensor = []
-    vocab = set()
+    vocab = set()  # Een set voor unieke woorden
+
+    # Verzamel alle woorden in vocab
+    for line in lines:
+        input_line, target_line = line.split('\t')
+        
+        input_tokens = input_line.split()
+        target_tokens = target_line.split()
+
+        vocab.update(input_tokens)
+        vocab.update(target_tokens)
+
+    # Maak vocab een gesorteerde lijst
+    vocab = sorted(vocab)
+
+    # Maak een woordenboek om snel indices op te halen
+    word_to_idx = {word: idx for idx, word in enumerate(vocab)}
 
     for line in lines:
         input_line, target_line = line.split('\t')
-        # Voeg numerieke waarden toe aan target_tensor en woorden aan vocab
-        target_tensor.append([int(re.sub(r'\D', '', word)) for word in target_line.split() if re.sub(r'\D', '', word) != ''])
-        vocab.update(input_line.split())
-        vocab.update(target_line.split())
 
-    vocab = {word: idx for idx, word in enumerate(sorted(vocab))}
-    
-    # Nu moeten we input_tensor als een lijst van indices maken
-    for line in lines:
-        input_line, _ = line.split('\t')
-        input_tensor.append([vocab[word] for word in input_line.split()])
+        input_tokens = input_line.split()
+        target_tokens = target_line.split()
 
-    # Zet input_tensor en target_tensor om naar tensors van het juiste type
-    input_tensor = torch.tensor(input_tensor, dtype=torch.long)
+        # Convert de input tokens naar indices
+        input_tensor.append(torch.tensor([word_to_idx.get(word, 0) for word in input_tokens], dtype=torch.long))
+
+        # Voeg numerieke waarden toe aan target_tensor
+        target_tensor.append([int(re.sub(r'\D', '', word)) for word in target_tokens if re.sub(r'\D', '', word) != ''])
+
+    # Pad de input_tensor zodat alle sequenties dezelfde lengte hebben
+    input_tensor = pad_sequence(input_tensor, batch_first=True, padding_value=0)
+
     target_tensor = torch.tensor(target_tensor, dtype=torch.long)
 
-    return input_tensor, target_tensor, vocab
+    return input_tensor, target_tensor, word_to_idx
